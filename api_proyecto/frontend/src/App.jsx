6e437ee03c1f9c02834458fc32b73b08e8307dc3
Juan Sepúlveda <sepujuan96@gmail.com>
2023-08-31 14:11:06 -0300

    Update App.jsx

--- api_proyecto/frontend/src/App.jsx
+++ api_proyecto/frontend/src/App.jsx
@@ -22,6 +22,8 @@ function App() {
   })
 
   const getAPI = async (url,setEstado,python = true) => {
+    //El parametro opcional PYTHON recibe un booleano que indica si la API a la que consultamos es 
+    //la nuestra en Python o es una externa (Marcelo / PHP). Por eso, la forma de setear el estado es diferente.
     const resAPI = await fetch(url)
     const resAPI2 = await resAPI.json()
     python ? setEstado(resAPI2.data) : setEstado(resAPI2)
