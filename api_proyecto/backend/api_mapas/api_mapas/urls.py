c4ff99d4197c62cc029ce36e54769172a320fa46
Juan Sepúlveda <sepujuan96@gmail.com>
2023-09-07 14:16:35 -0300

    Back

--- api_proyecto/backend/api_mapas/api_mapas/urls.py
+++ api_proyecto/backend/api_mapas/api_mapas/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from apps.api import views
 from django.contrib import admin
-from apps.api.views import filterbyParams, getErrorAddress, getIncompletesAddress,syncAPIViewSet
+from apps.api.views import filterbyParams, getErrorAddress, getIncompletesAddress,syncAPIViewSet, OficiosViewSet
 
 
 router = DefaultRouter()
@@ -11,6 +11,9 @@ router.register(r'getAddress',views.AdressesViewSet, basename='getAddress')
 router.register(r'filterbyParams',views.filterbyParams, basename='filterbyParams')
 router.register(r'getErrorAddress',views.getErrorAddress, basename='getErrorAddress')
 router.register(r'getIncompletesAddress',views.getIncompletesAddress, basename='getIncompletesAddress')
+router.register(r'oficios', views.OficiosViewSet, basename='getOficios')
+router.register(r'listUniqueAddressNames', views.filterbyParams, basename='listUniqueAddressNames')
+router.register(r'registerOficio', views.filterbyParams, basename='registerOficio')
 
 
 urlpatterns = [ 
@@ -18,9 +21,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('sync/',syncAPIViewSet.as_view({'get': 'sync_addresses'}), name='sync_addresses'),
     path('filterbyParams/<str:codigo_postal>/<str:calle>/<str:altura>',
-        filterbyParams.as_view({'get': 'list'}), name='filterbyParams'),
+        filterbyParams.as_view({'get': 'search'}), name='filterbyParams'),
     path('getErrorAddress/',
         getErrorAddress.as_view({'get': 'list'}), name='getErrorAddress'),
     path('getIncompletesAddress/',
         getIncompletesAddress.as_view({'get': 'list'}), name='getIncompletesAddress'),
+    path('oficios/',
+        OficiosViewSet.as_view({'get': 'list'}), name='getOficios'),
+    path('listUniqueAddressNames/',
+        filterbyParams.as_view({'get': 'listUniqueAddressNames'}), name='listUniqueAddressNames'),
+    path('registerOficio/',
+        filterbyParams.as_view({'post': 'registerOficio'}), name='registerOficio'),
 ]
--- api_proyecto/backend/api_mapas/apps/api/serializer.py
+++ api_proyecto/backend/api_mapas/apps/api/serializer.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Address
+from .models import Address, Oficios
 
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
+        fields = '__all__'
+        
+class OficiosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Oficios
         fields = '__all__'
